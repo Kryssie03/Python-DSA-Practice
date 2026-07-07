fb2283e91d3d6235d0ae254bac36e293fa377f5f
Christabel-ML <kosisochukwuakabekwa@gmail.com>
2026-07-07 17:54:58 +0100

    Function to reverse array in groups

--- Array/rev_array_group.py
+++ Array/rev_array_group.py
@@ -4,12 +4,12 @@ def rev_array(arr: list,k: int)-> list:
   i = 0
   n = len(arr)
   while i < n:
-    left = i
-    right = min(i + k - 1, n - 1)
-    while left < right:
-      arr[left], arr[right] = arr[right], arr[left]
-      left += 1
-      right -= 1
+    l = i
+    r = min(i + k - 1, n - 1)
+    while l < r:
+      arr[l], arr[r] = arr[r], arr[l]
+      l += 1
+      r -= 1
     i += k
   return arr
 h = rev_array([1,2,3,4,5,6,7,8],8)
